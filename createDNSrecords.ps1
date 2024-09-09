3f026b38b0da2558d36899716d4ef132aefcf93d
CruzBenny <cruzbo@etpi.com.ph>
2024-09-09 15:56:50 +0800

    Update createDNSrecords.ps1

--- createDNSrecords.ps1
+++ createDNSrecords.ps1
@@ -1,17 +1,17 @@
 ﻿Install-WindowsFeature  -name dns  -includeManagementTools
-Add-DnsServerPrimaryZone -Name "ngcpM.ph" -ZoneFile "ngcpM.ph.dns"
-add-DnsServerResourceRecord -zonename ngcpM.ph -A -name ns  -ipv4address 10.m.1.10
-add-DnsServerResourceRecord -zonename ngcpM.ph -Cname -name www -hostname ns.ngcpM.ph
-add-DnsServerResourceRecord -zonename ngcpM.ph -Cname -name imap -hostname ns.ngcpM.ph
-add-DnsServerResourceRecord -zonename ngcpM.ph -Cname -name pop -hostname ns.ngcpM.ph
-add-DnsServerResourceRecord -zonename ngcpM.ph -Cname -name smtp -hostname ns.ngcpM.ph
+Add-DnsServerPrimaryZone -Name "CCNA41.com" -ZoneFile "CCNA41.com.dns"
+add-DnsServerResourceRecord -zonename CCNA41.com -A -name ns  -ipv4address 10.41.1.10
+add-DnsServerResourceRecord -zonename CCNA41.com -Cname -name www -hostname ns.CCNA41.com
+add-DnsServerResourceRecord -zonename CCNA41.com -Cname -name imap -hostname ns.CCNA41.com
+add-DnsServerResourceRecord -zonename CCNA41.com -Cname -name pop -hostname ns.CCNA41.com
+add-DnsServerResourceRecord -zonename CCNA41.com -Cname -name smtp -hostname ns.CCNA41.com
 ###FOR CISCO DEVICES DNS RECORDS;
-add-DnsServerResourceRecord -zonename ngcpM.ph -A -name cb  -ipv4address 10.m.1.4
-add-DnsServerResourceRecord -zonename ngcpM.ph -A -name ct  -ipv4address 10.m.1.2
-add-DnsServerResourceRecord -zonename ngcpM.ph -A -name cm  -ipv4address 10.m.100.8
-add-DnsServerResourceRecord -zonename ngcpM.ph -A -name ed  -ipv4address 10.m.m.1
-add-DnsServerResourceRecord -zonename ngcpM.ph -A -name p1  -ipv4address 10.m.100.101
-add-DnsServerResourceRecord -zonename ngcpM.ph -A -name p2  -ipv4address 10.m.100.102
-add-DnsServerResourceRecord -zonename ngcpM.ph -A -name c1  -ipv4address 10.m.50.6
-add-DnsServerResourceRecord -zonename ngcpM.ph -A -name c2  -ipv4address 10.m.50.8
-add-DnsServerResourceRecord -zonename ngcpM.ph -A -name ap  -ipv4address 10.m.10.3
+add-DnsServerResourceRecord -zonename CCNA41.com -A -name cb  -ipv4address 10.41.1.4
+add-DnsServerResourceRecord -zonename CCNA41.com -A -name ct  -ipv4address 10.41.1.2
+add-DnsServerResourceRecord -zonename CCNA41.com -A -name cm  -ipv4address 10.41.100.8
+add-DnsServerResourceRecord -zonename CCNA41.com -A -name ed  -ipv4address 10.41.41.1
+add-DnsServerResourceRecord -zonename CCNA41.com -A -name p1  -ipv4address 10.41.100.101
+add-DnsServerResourceRecord -zonename CCNA41.com -A -name p2  -ipv4address 10.41.100.102
+add-DnsServerResourceRecord -zonename CCNA41.com -A -name c1  -ipv4address 10.41.50.6
+add-DnsServerResourceRecord -zonename CCNA41.com -A -name c2  -ipv4address 10.41.50.8
+add-DnsServerResourceRecord -zonename CCNA41.com -A -name ap  -ipv4address 10.41.10.3
